c710765c31914c7ebdf543582644e1fa090161d5
Zouhaier SBAITI <zouhaier36@gmail.com>
2019-05-09 14:03:14 +0100

    hi

--- backoffice/src/App.js
+++ backoffice/src/App.js
@@ -1,21 +1,18 @@
 import React, { Component } from 'react';
 import Login from './components/login/Login';
 import Home from './components/home/Home';
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Redirect, Route, Switch, Router } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <Router>
-          <div>
-            <Switch>
-              <Route exact path="/login" component={Login} />
-              <Route path="/home" component={Home} />
-            </Switch>
-          </div>
-        </Router>
-      </div>
+      <Router>
+        <Switch>
+          <Route exact path="/login" component={Login} />
+          <Route path="/home" component={Home} />
+          <Redirect from='/' exact to='/login' />
+        </Switch>
+      </Router>
     );
   }
 }
